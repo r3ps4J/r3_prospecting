b8dbfec6330c4e95d05db0f19a95ad9daef08abc
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 02:50:54 +0200

    Move item files to items subfolder

--- r3_prospecting.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('bones', 'Bones', 100, 0, 1),
-	('detector', 'Metal Detector', 2000, 0, 1),
-	('dragon_scales', 'Dragon Scales', 1000, 0, 1),
-	('gold_ring', 'Gold Ring', 100, 0, 1),
-	('metalscrap', 'Metal Scrap', 100, 0, 1),
-	('nuts_and_bolts', 'Nuts and Bolts', 100, 0, 1);
