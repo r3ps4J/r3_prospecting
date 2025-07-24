ec121b0ce965374f30ff9377c2b8df0802189292
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-24 20:04:01 +0200

    Update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,21 @@
-name "r3_prospecting"
-author "r3ps4J"
-contact "discord.gg/bEWmBbg"
-
 fx_version "adamant"
 game "gta5"
 
+name "r3_prospecting"
 description "Prospecting plugin"
+author "r3ps4J"
+contact "discord.gg/bEWmBbg"
 
 dependencies {
     "prospecting",
     "r3_servicesmanager",
 }
-server_script "@prospecting/interface.lua"
+
+shared_script "config.lua"
 
 client_script "scripts/cl_*.lua"
-server_script "scripts/sv_*.lua"
 
-client_script "config.lua"
-server_script "config.lua"
+server_scripts {
+    "@prospecting/interface.lua",
+    "scripts/sv_*.lua",
+}
