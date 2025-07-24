8aab4039d60715f20a1ed610f24e5dbf0c20bf6e
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-24 20:24:36 +0200

    Add more values to config

--- scripts/cl_prospect.lua
+++ scripts/cl_prospect.lua
@@ -1,44 +1,39 @@
 local notificationProvider = exports.r3_servicesmanager:load("notification")
 
-local blip_location = vector3(1580.9, 6592.204, 13.84828)
-local blip = nil
-local area_blip = nil
-local area_size = 100.0
-
 CreateThread(function()
-    AddTextEntry("PROSP_BLIP", Config.ProspectingBlipText)
-    blip = AddBlipForCoord(blip_location)
-    SetBlipSprite(blip, Config.ProspectingBlipSprite)
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName("PROSP_BLIP")
-    EndTextCommandSetBlipName(blip)
-    area_blip = AddBlipForRadius(blip_location, area_size)
-    SetBlipSprite(area_blip, 10)
+	AddTextEntry("PROSP_BLIP", Config.blip.text)
+	local blip = AddBlipForCoord(config.baseLocation)
+	SetBlipSprite(blip, Config.blip.sprite)
+	SetBlipAsShortRange(blip, true)
+	BeginTextCommandSetBlipName("PROSP_BLIP")
+	EndTextCommandSetBlipName(blip)
+	local areaBlip = AddBlipForRadius(Config.baseLocation, Config.areaSize)
+	SetBlipSprite(areaBlip, 10)
 end)
 
 RegisterNetEvent("r3_prospecting:startProspecting")
 AddEventHandler("r3_prospecting:startProspecting", function()
-    local pos = GetEntityCoords(PlayerPedId())
+	local pos = GetEntityCoords(PlayerPedId())
 
-    -- Make sure the player is within the prospecting zone before they start
-    local dist = #(pos - blip_location)
-    if dist < area_size then
-        TriggerServerEvent("r3_prospecting:activateProspecting")
-    else
-        notificationProvider.showNotification("You are not in a prospecting area!", {
-            style = "error",
-            duration = 5000,
-        })
+	-- Make sure the player is within the prospecting zone before they start
+	local dist = #(pos - Config.baseLocation)
+	if dist < Config.areaSize then
+		TriggerServerEvent("r3_prospecting:activateProspecting")
+	else
+		notificationProvider.showNotification("You are not in a prospecting area!", {
+			style = "error",
+			duration = 5000,
+		})
 	end
 end, false)
 
 RegisterNetEvent("r3_prospecting:useDetector")
 AddEventHandler("r3_prospecting:useDetector", function()
 	if IsPedInAnyVehicle(PlayerPedId()) then
-        notificationProvider.showNotification("You cannot prospect from a vehicle!", {
-            style = "error",
-            duration = 5000,
-        })
+		notificationProvider.showNotification("You cannot prospect from a vehicle!", {
+			style = "error",
+			duration = 5000,
+		})
 	else
 		TriggerEvent("r3_prospecting:startProspecting")
 	end
--- scripts/sv_locations.lua
+++ scripts/sv_locations.lua
@@ -2,57 +2,22 @@ local notificationProvider = exports.r3_servicesmanager:load("notification")
 local inventoryProvider = exports.r3_servicesmanager:load("inventory")
 local usableItemsProvider = exports.r3_servicesmanager:load("usableItems")
 
-local locations = {
-    {x = 1600.185, y = 6622.714, z = 15.85106, data = {
-        item = "bones",
-		label = "Bones",
-    }},
-    {x = 1548.082, y = 6633.096, z = 2.377085, data = {
-        item = "nuts_and_bolts",
-		label = "Nuts and Bolts"
-    }},
-    {x = 1504.235, y = 6579.784, z = 4.365892, data = {
-        item = "gold_ring",
-        label = "a Golden ring",
-    }},
-    {x = 1580.016, y = 6547.394, z = 15.96557, data = {
-        item = "dragon_scales",
-        label = "Dragon Scales",
-    }},
-    {x = 1634.586, y = 6596.688, z = 22.55633, data = {
-        item = "metalscrap",
-		label = "Metal Scrap",
-    }},
-}
-
-local item_pool = {
-    {item = "bones", label = "Bones"},
-    {item = "nuts_and_bolts", label = "Nuts and Bolts"},
-    {item = "gold_ring", label = "a Golden ring"},
-    {item = "dragon_scales", label = "Dragon Scales"},
-    {item = "metalscrap", label = "Metal Scrap"},
-}
-
--- Area to create targets within, matches the client side blips
-local base_location = vector3(1580.9, 6592.204, 13.84828)
-local area_size = 100.0
-
 -- Choose a random item from the item_pool list
 local function getNewRandomItem()
-    local item = item_pool[math.random(#item_pool)]
+    local item = Config.itemPool[math.random(#Config.itemPool)]
     return {item = item.item, label = item.label}
 end
 
 -- Make a random location within the area
 local function getNewRandomLocation()
-    local offsetX = math.random(-area_size, area_size)
-    local offsetY = math.random(-area_size, area_size)
+    local offsetX = math.random(-Config.areaSize, Config.areaSize)
+    local offsetY = math.random(-Config.areaSize, Config.areaSize)
     local pos = vector3(offsetX, offsetY, 0.0)
-    if #(pos) > area_size then
+    if #(pos) > Config.areaSize then
         -- It's not within the circle, generate a new one instead
         return getNewRandomLocation()
     end
-    return base_location + pos
+    return Config.baseLocation + pos
 end
 
 -- Generate a new target location
@@ -86,12 +51,8 @@ CreateThread(function()
     -- Default difficulty
     Prospecting.SetDifficulty(1.0)
 
-    -- Add a list of targets
-    -- Each target needs an x, y, z and data entry
-    Prospecting.AddTargets(locations)
-
-    -- Generate 10 random extra targets
-    for n = 0, 10 do
+    -- Generate random extra targets
+    for n = 0, Config.initialTargets do
         generateNewTarget()
     end
 
@@ -120,6 +81,6 @@ CreateThread(function()
     end)
 end)
 
-usableItemsProvider.registerUsableItem("detector", function(source)
+usableItemsProvider.registerUsableItem(Config.detectorItem, function(source)
 	TriggerClientEvent("r3_prospecting:useDetector", source)
 end)
